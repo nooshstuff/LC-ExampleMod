7780d73d7139a46b9c8f17c0465e2997e89cb069
Noosh <nooshi34@gmail.com>
2023-12-30 18:04:41 -0500

    Add project files.

---
+++ PlayerSpeedPatch.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine; // you would need this to do stuff like get Time.deltatime
+using GameNetcodeStuff;
+
+namespace NooshMod
+{
+	[HarmonyPatch(typeof(PlayerControllerB))]	// if you didnt do this up here you could do it on the indivual functions. when you do this you cant patch other classes
+	internal class PlayerSpeedPatch // having classes related to patches have the name Patch might be useful
+	{
+		[HarmonyPostfix] // theres also [HarmonyPrefix]
+		[HarmonyPatch(nameof(PlayerControllerB.Update))] // every patch needs a class type and class name. this could also be a string but whatever too complicated
+		public static void ApplyBigJumpValue( PlayerControllerB __instance) // double underscore is a naming convention https://harmony.pardeike.net/articles/patching-injections.html
+		{
+			var dt = Time.deltaTime;
+			__instance.jumpForce = 100;	// this is a float but its implicitly cast. remember to use f when you're using decimals in math
+		}
+	}
+}
---
+++ SamplePlugin.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace NooshMod
+{
+	[BepInPlugin(GeneratedPluginInfo.Identifier, GeneratedPluginInfo.Name, GeneratedPluginInfo.Version)]
+	public sealed class SamplePlugin : BaseUnityPlugin
+	{
+		private void Awake()
+		{
+			// Plugin startup logic
+			Logger.LogInfo($"Plugin {GeneratedPluginInfo.Identifier} is loaded!");
+			Harmony.CreateAndPatchAll(System.Reflection.Assembly.GetExecutingAssembly(), GeneratedPluginInfo.Identifier); //this applies everything in this project with HarmonyPatch
+
+			// NOW FOR MONOMOD
+			On.GameNetcodeStuff.PlayerControllerB.Update += PlayerControllerB_Update_Funnybusiness; // this is kinda cool and kinda silly
+		}
+
+		private void PlayerControllerB_Update_Funnybusiness(On.GameNetcodeStuff.PlayerControllerB.orig_Update orig, GameNetcodeStuff.PlayerControllerB self)
+		{
+			orig(self); // this is like super.update() in other langauges. code's relative location to this is like doing prefix and postfix
+			if (self.isSprinting) {
+				float downwardsAngle = Vector3.Angle(self.playerEye.transform.forward, Vector3.down); // angle btween the two
+				Logger.LogInfo($"Player downwards angle = {downwardsAngle}, Ramming Speed = {(downwardsAngle < 45.0f)}");
+				if (downwardsAngle < 45.0f) {
+					self.sprintMultiplier = Mathf.Lerp(self.sprintMultiplier, 10.0f, Time.deltaTime * 1f); // funny murder miners (max sprintmult is now 10.0!)
+				}
+				else { self.sprintMultiplier = Mathf.Lerp(self.sprintMultiplier, 2.25f, Time.deltaTime * 1f); }
+			}
+			else { self.sprintMultiplier = Mathf.Lerp(self.sprintMultiplier, 1f, 10f * Time.deltaTime); }
+		}
+	}
+}
